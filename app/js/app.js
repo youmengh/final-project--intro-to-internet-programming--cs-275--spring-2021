b26d354235e65e2acfd890bc58e6063c7263961b
Roy A Vanegas <roy@thecodeeducators.com>
2021-04-26 15:50:37 -0400

    Add a line break

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,4 @@
 window.onload = () => {
-    console.log(`%c JavaScript works!`, 'background-color: yellow; font-weight: bold;');
+    console.log(`%c JavaScript works!`,
+        `background-color: yellow; font-weight: bold;`);
 };
