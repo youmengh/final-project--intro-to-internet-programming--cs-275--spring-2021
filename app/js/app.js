bbbc5df51957dab9f6ec1affe9a03b721832af2e
Youmeng Hin <youmenghin@gmail.com>
2021-05-12 22:56:14 -0400

    Fixed gulp errors

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,18 @@
 window.onload = () => {
-
+    
     let runAnimation = () => {
-        let box = document.getElementById("diamond-container");
+
+        let box = document.getElementById(`diamond-container`);
         let styleLeft = 0;
         let rightMost = false;
         box.style.color = `blue`;
         box.style.border = `2px solid blue`;
 
         let slide = () => {
-            let border = document.querySelector(`html`).clientWidth - document.getElementById("diamond-container").clientWidth;
+
+            let htmlWidth = document.querySelector(`html`).clientWidth;
+            let boxWidth = document.getElementById(`diamond-container`).clientWidth;
+            let border = htmlWidth - boxWidth;
 
             //When left offset is less than border and
             //diamond has not yet hit the right most
@@ -34,103 +38,104 @@ window.onload = () => {
             if (rightMost) {
                 styleLeft--;
             }
+
             box.style.left = styleLeft + `px`;
 
         };
 
         setInterval(slide, 1);
 
-    }
+    };
 
     let drawDiamond = (size) => {
 
-        if (size % 2 == 0) {        //If size is even, prints even-sized diamond
+        if (size % 2 === 0) {        //If size is even, prints even-sized diamond
 
             let numStar = 0.5;
             let numSpace = size / 2;
-
-            let x = document.getElementById("diamond-container");
-            x.innerHTML = "";
+            
+            let x = document.getElementById(`diamond-container`);
+            x.innerHTML = ``;
 
             //Prints top half of diamond
             for (let row = 0; row <= (size / 2); row++) {
                 for (let col = 0; col < numSpace; col++) {            //Prints spaces
-                    x.innerHTML += "&nbsp;&nbsp;&nbsp;&nbsp;";
+                    x.innerHTML += `&nbsp;&nbsp;&nbsp;&nbsp;`;
                 }
                 numSpace--;
                 numStar = row;
                 if (numStar < 1) {
-                    x.innerHTML += "*";                         //Prints stars
+                    x.innerHTML += `*`;                         //Prints stars
                 } else {
                     for (let col = 0; col < numStar; col++) {         //Prints stars
-                        x.innerHTML += "&nbsp;*&nbsp;";
+                        x.innerHTML += `&nbsp;*&nbsp;`;
                     }
-                    x.innerHTML += "&nbsp;&nbsp;";
+                    x.innerHTML += `&nbsp;&nbsp;`;
                     for (let col = 0; col < numStar; col++) {         //Prints stars
-                        x.innerHTML += "&nbsp;*&nbsp;";
+                        x.innerHTML += `&nbsp;*&nbsp;`;
                     }
                 }
-                x.innerHTML += "<br />";
+                x.innerHTML += `<br />`;
             }
             numSpace = 1;
             //Prints bottom half of diamond
             for (let row = 0; row < (size / 2); row++) {
                 for (let col = 0; col < numSpace; col++) {            //Prints spaces
-                    x.innerHTML += "&nbsp;&nbsp;&nbsp;&nbsp;";
+                    x.innerHTML += `&nbsp;&nbsp;&nbsp;&nbsp;`;
                 }
                 numStar = size - (numSpace * 2);
                 numSpace++;
                 if (numStar < 1) {                              //Prints stars
-                    x.innerHTML += "*";
+                    x.innerHTML += `*`;
                 } else {
                     for (let col = 0; col < (numStar / 2); col++) {   //Prints stars
-                        x.innerHTML += "&nbsp;*&nbsp;";
+                        x.innerHTML += `&nbsp;*&nbsp;`;
                     }
-                    x.innerHTML += "&nbsp;&nbsp;";
+                    x.innerHTML += `&nbsp;&nbsp;`;
                     for (let col = 0; col < (numStar / 2); col++) {   //Prints stars
-                        x.innerHTML += "&nbsp;*&nbsp;";
+                        x.innerHTML += `&nbsp;*&nbsp;`;
                     }
                 }
-                x.innerHTML += "<br / >";
+                x.innerHTML += `<br / >`;
             }
         } else {        //If size is odd, prints odd-sized diamond
 
             let numStar = 0;
             let numSpace = (size - 1) / 2;
 
-            let x = document.getElementById("diamond-container");
-            x.innerHTML = "";
+            let x = document.getElementById(`diamond-container`);
+            x.innerHTML = ``;
 
             //Prints top half of diamond
             for (let row = 0; row < (size / 2); row++) {
                 for (let col = 0; col < numSpace; col++) {       //Prints spaces
-                    x.innerHTML += "&nbsp;&nbsp;&nbsp;&nbsp;";
+                    x.innerHTML += `&nbsp;&nbsp;&nbsp;&nbsp;`;
                 }
                 numSpace--;
                 numStar = (row * 2) + 1;
                 for (let col = 0; col < numStar; col++) {              //Prints stars
-                    x.innerHTML += "&nbsp;*&nbsp;";
+                    x.innerHTML += `&nbsp;*&nbsp;`;
                 }
-                x.innerHTML += "<br />";
+                x.innerHTML += `<br />`;
             }
             numSpace = 1;
             //Prints bottom half of diamond
             for (let row = 1; row <= (size - 1) / 2; row++) {
                 for (let col = 0; col < numSpace; col++) {       //Prints spaces
-                    x.innerHTML += "&nbsp;&nbsp;&nbsp;&nbsp;";
+                    x.innerHTML += `&nbsp;&nbsp;&nbsp;&nbsp;`;
                 }
                 numSpace++;
                 numStar = size - (row * 2);
                 for (let col = 0; col < numStar; col++) {              //Prints stars
-                    x.innerHTML += "&nbsp;*&nbsp;";
+                    x.innerHTML += `&nbsp;*&nbsp;`;
                 }
-                x.innerHTML += "<br />";
+                x.innerHTML += `<br />`;
             }
         }
-    }
+    };
 
-    let user = prompt("Enter the size of diamond: ", "1");
+    let user = prompt(`Enter the size of diamond: `, `1`);
     let size = parseInt(user);
     drawDiamond(size);
     runAnimation();
-}
+};
